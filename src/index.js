d1974286b74f2995f7ac0968f594679359bbc80b
Student-Java <energoslav@gmail.com>
2020-04-07 14:42:58 -0300

    feat: add drawing functions and key events handlers

--- src/index.js
+++ src/index.js
@@ -1,28 +1,130 @@
 import './assets/style.css';
-import { draw } from './draw';
+import { drawKeyboard } from './drawKeyboard';
+import { config, defaultValues } from './config/config';
+import { redrawLetters } from '@/renderLetters';
 
+
+const state = {
+  CapsLock: false,
+  shift: false,
+  ControlLeft: false,
+  lang: defaultValues.lang
+}
 
 const body = document.querySelector('body');
 const content = document.createElement('section');
 content.classList.add('content')
+body.append(content);
 const textarea = document.createElement('textarea');
 content.append(textarea);
 const keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
-keyboard.addEventListener('click', (e) => e.target.id && console.log(e.target));
+keyboard.addEventListener('click', (e) => {
+  if (e.target.id) {
+    document
+      .querySelector('body')
+      .dispatchEvent(
+        new CustomEvent(
+          'keydown',
+          {
+            detail: {
+              code: e.target.id,
+              key: config[e.target.id].en
+            }
+          }
+        )
+      );
+  }
+});
 
 
 const onLoad = () => {
-  draw(keyboard);
   content.append(keyboard);
+  drawKeyboard(keyboard);
+  redrawLetters(state);
 };
 
+document.addEventListener('DOMContentLoaded', onLoad);
 
-const b = document.createElement('button');
-b.innerText = 'LOAD';
-body.append(b, content);
+const getFlash = () => {
+  let flash = document.createElement('div');
+  flash.classList.add('flash', 'lighting');
+  // flash.style = 'height: 60px;width: 40px;top: 0;left: -140px;'
 
+  return flash;
+};
 
-// document.addEventListener("DOMContentLoaded", onLoad);
-b.addEventListener('click', onLoad);
-body.addEventListener('keydown', (e) => textarea.value = textarea.value + e.code + ' ');
+const activateButton = code => {
+  const button = document.getElementById(code);
+  if (!button.classList.contains('active')) {
+    button.classList.add('active', 'autoflash');
+    const flash = getFlash();
+    button.append(flash);
+    console.log(flash);
+  }
+};
+
+const deactivateButton = code => {
+  const button = document.getElementById(code);
+  if (button.classList.contains('active')) {
+    button.classList.remove('active', 'autoflash');
+    button.removeChild(button.querySelector('div'));
+  }
+};
+
+const normalizeShift = code => code.indexOf('Shift') !== -1 ? 'Shift' : code;
+
+const extractCode = e => normalizeShift(e.detail ? e.detail.code : e.code);
+
+body.addEventListener('keydown', (e) => {
+    const code = extractCode(e);
+
+    if (!e.metaKey) {
+      e.preventDefault();
+    }
+
+    if (!Object.keys(config).some(k => k === code)) {
+      return;
+    }
+
+    if (code === 'Shift') {
+      state.shift = true;
+    } else if (code === 'CapsLock') {
+      state.CapsLock = !state.CapsLock;
+    } else if (code === 'ControlLeft') {
+      state.ControlLeft = true;
+    }
+
+    activateButton(code);
+
+    textarea.value = textarea.value + code + ' ';
+
+    redrawLetters(state);
+  }
+);
+
+const changeLang = code => {
+  if (state.shift && state.ControlLeft && (code === 'Shift' || code === 'ControlLeft')) {
+    state.lang = (state.lang === defaultValues.lang ? defaultValues.altLang : defaultValues.lang);
+  }
+};
+
+body.addEventListener('keyup', (e) => {
+    const code = extractCode(e);
+    if (!Object.keys(config).some(k => k === code)) {
+      return;
+    }
+
+    changeLang(code);
+
+    if (code === 'Shift') {
+      state.shift = false;
+    } else if (code === 'ControlLeft') {
+      state.ControlLeft = false;
+    }
+
+    deactivateButton(code);
+
+    redrawLetters(state);
+  }
+);
---
+++ src/renderLetters.js
@@ -0,0 +1,25 @@
+import { config } from './config/config';
+
+
+const getGliffFromConfig = (value, state) => {
+  let gliff;
+  const rawGliff = value[state.lang];
+
+  if (value.special) {
+    gliff = rawGliff;
+  } else if (Array.isArray(rawGliff)) {
+    gliff = rawGliff[Number(state.shift)];
+  } else {
+    const upperCase = state.shift ? !state.CapsLock : state.CapsLock;
+    gliff = upperCase ? rawGliff.toUpperCase() : rawGliff;
+  }
+  return gliff;
+};
+
+export const redrawLetters = state => {
+  Object.entries(config).forEach(([key, value]) => {
+      const button = document.getElementById(key);
+      button.querySelector('span').innerText = getGliffFromConfig(value, state);
+    }
+  );
+}
